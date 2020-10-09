6046b1b9696b47c0fd55eb9cd620c57513e6a28d
Kayhwee <limkayhwee@hotmail.com>
2020-10-09 21:30:16 +0800

    Updated menu items to navigate using withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -21,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     </div>
 );
 
-export default withRouter(MenuItem);
+export default withRouter(MenuItem); // Higher order component
